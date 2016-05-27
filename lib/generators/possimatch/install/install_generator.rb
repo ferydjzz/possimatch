797680447024e52cbb0d643b31759b56d508ea61
FeryJ <fery.japminto@jurnal.id>
2016-05-27 14:39:58 +0700

    move migration patch

--- lib/generators/possimatch/install/install_generator.rb
+++ lib/generators/possimatch/install/install_generator.rb
@@ -6,12 +6,12 @@ module Possimatch
       include Rails::Generators::Migration
       source_root File.expand_path("../templates", __FILE__)
 
-      desc 'Add possimatch migrations'
+      def create_migrations
+        desc 'Add possimatch migrations'
 
-      def initialize
-        migration_template 'templates/migrations/create_possible_sources.rb', 'db/migrate/create_possible_sources.rb'
-        migration_template 'templates/migrations/create_possible_rules.rb', 'db/migrate/create_possible_rules.rb'
-        migration_template 'templates/migrations/create_possible_matches.rb', 'db/migrate/create_possible_matches.rb'
+        migration_template 'migrations/create_possible_sources.rb', 'db/migrate/create_possible_sources.rb'
+        migration_template 'migrations/create_possible_rules.rb', 'db/migrate/create_possible_rules.rb'
+        migration_template 'migrations/create_possible_matches.rb', 'db/migrate/create_possible_matches.rb'
       end
     end
   end
