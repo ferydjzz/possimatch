6f0eb055064ce799f40f314a6cb7b608d1dd0f37
FeryJ <fery.japminto@jurnal.id>
2016-05-27 15:15:59 +0700

    test w/o migration number

--- lib/generators/possimatch/install/install_generator.rb
+++ lib/generators/possimatch/install/install_generator.rb
@@ -5,10 +5,21 @@ module Possimatch
     class InstallGenerator < ActiveRecord::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
+      desc "create Possimatch migrations"
+
+      # def self.next_migration_number(path)
+      #   unless @prev_migration_nr
+      #     @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+      #   else
+      #     @prev_migration_nr += 1
+      #   end
+      #   @prev_migration_nr.to_s
+      # end
+
       def create_migrations
-        migration_template 'migrations/create_possible_sources.rb', 'db/migrate/create_possible_sources.rb'
-        migration_template 'migrations/create_possible_rules.rb', 'db/migrate/create_possible_rules.rb'
-        migration_template 'migrations/create_possible_matches.rb', 'db/migrate/create_possible_matches.rb'
+        migration_template 'migrations/create_possi_sources.rb', 'db/migrate/create_possi_sources.rb'
+        migration_template 'migrations/create_possi_rules.rb', 'db/migrate/create_possi_rules.rb'
+        migration_template 'migrations/create_possi_matches.rb', 'db/migrate/create_possi_matches.rb'
       end
     end
   end
