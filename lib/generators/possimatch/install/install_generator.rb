f8d214e0f668c1d0c2ec8317c416a7d219ea0776
FeryJ <fery.japminto@jurnal.id>
2016-05-27 15:27:44 +0700

    test with number

--- lib/generators/possimatch/install/install_generator.rb
+++ lib/generators/possimatch/install/install_generator.rb
@@ -7,16 +7,18 @@ module Possimatch
 
       desc "create Possimatch migrations"
 
-      # def self.next_migration_number(path)
-      #   unless @prev_migration_nr
-      #     @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-      #   else
-      #     @prev_migration_nr += 1
-      #   end
-      #   @prev_migration_nr.to_s
-      # end
+      def self.next_migration_number(path)
+        unless @prev_migration_nr
+          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        else
+          @prev_migration_nr += 1
+        end
+        @prev_migration_nr.to_s
+      end
 
       def create_migrations
+        desc "copy migrations"
+        
         migration_template 'migrations/create_possi_sources.rb', 'db/migrate/create_possi_sources.rb'
         migration_template 'migrations/create_possi_rules.rb', 'db/migrate/create_possi_rules.rb'
         migration_template 'migrations/create_possi_matches.rb', 'db/migrate/create_possi_matches.rb'
