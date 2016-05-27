c77a12ff99cc2adbe8e4485afe1a758c85ed1d71
FeryJ <fery.japminto@jurnal.id>
2016-05-27 15:06:18 +0700

    lalala, change to active record generators

--- lib/generators/possimatch/install/install_generator.rb
+++ lib/generators/possimatch/install/install_generator.rb
@@ -1,14 +1,11 @@
-require 'rails/generators/migration'
+require 'rails/generators/active_record'
 
 module Possimatch
   module Generators
     class InstallGenerator < ActiveRecord::Generators::Base
-      include Rails::Generators::Migration
       source_root File.expand_path("../templates", __FILE__)
 
       def create_migrations
-        desc 'Add possimatch migrations'
-
         migration_template 'migrations/create_possible_sources.rb', 'db/migrate/create_possible_sources.rb'
         migration_template 'migrations/create_possible_rules.rb', 'db/migrate/create_possible_rules.rb'
         migration_template 'migrations/create_possible_matches.rb', 'db/migrate/create_possible_matches.rb'
