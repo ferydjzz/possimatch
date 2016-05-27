1d8bbfc35e05ccd8f084b49701b425c86063d6bc
FeryJ <fery.japminto@jurnal.id>
2016-05-27 13:59:10 +0700

    fix migration template path

--- lib/generators/possimatch/install_generator.rb
+++ lib/generators/possimatch/install_generator.rb
@@ -3,13 +3,14 @@ require 'rails/generators/active_record'
 module Possimatch
   module Generators
     class InstallGenerator < ActiveRecord::Generators::Base
-      desc 'Create possimatch initializer'
-      
-      source_root File.expand_path("../../templates", __FILE__)
+      include Rails::Generators::Migration
+      source_root File.expand_path("../templates", __FILE__)
 
-      def create_migrations
-      	migration_template '../templates/migrations/create_possible_sources.rb', 'db/migrate/create_possible_sources.rb'
-      	migration_template '../templates/migrations/create_possible_rules.rb', 'db/migrate/create_possible_rules.rb'
+      desc 'Create possimatch initializer'
+
+      def copy_migrations
+        migration_template '../templates/migrations/create_possible_sources.rb', 'db/migrate/create_possible_sources.rb'
+        migration_template '../templates/migrations/create_possible_rules.rb', 'db/migrate/create_possible_rules.rb'
         migration_template '../templates/migrations/create_possible_matches.rb', 'db/migrate/create_possible_matches.rb'
       end
     end
