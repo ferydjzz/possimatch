39b015007aed14655588a7a06b0f5be509a40fa3
FeryJ <fery.japminto@jurnal.id>
2016-06-02 15:18:29 +0700

    add file to read configuration

--- lib/possimatch.rb
+++ lib/possimatch.rb
@@ -1,5 +1,11 @@
 require "possimatch/version"
+require "possimatch/helpers/configuration"
 
 module Possimatch
-  # Your code goes here...
+
+  # for reading config file in initializer
+  extend Configuration
+  define_setting :possible_matches, 3
+  define_setting :stop_after_match, false
+
 end
