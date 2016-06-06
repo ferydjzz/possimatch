09730e9739999915d587353b639a260c33e60ebf
FeryJ <fery.japminto@jurnal.id>
2016-06-06 19:24:28 +0700

    try n error

--- lib/generators/possimatch/resource/resource_generator.rb
+++ lib/generators/possimatch/resource/resource_generator.rb
@@ -10,12 +10,16 @@ module Possimatch
 
       source_root File.expand_path("../templates", __FILE__)
 
-      argument :attributes, :type => :array, :default => [], :banner => "from_class[:class] to_class[:class] group_key[:field]"
+      argument :attributes, :type => :array, :default => [], :banner => "<class_name>[:from_class] <class_name>[:to_class] <group_id>[:group_key]"
 
       def generate_config_file
+        from_class = attributes.select{ |a| a.type == "from_class"}
+        to_class = attributes.select{ |a| a.type == :to_class}
+        group_key = attributes.select{ |a| a.type == :"group_key"}
+
         puts "#{attributes.each{ |a| [a.name] }}"
-        puts "#{attributes.each{ |a| [a.attr_options[:from_class], a.attr_options[:to_class], a.attr_options[:group_key]] }}"
-        @boilerplate = Possimatch::Generators::Boilerplate.new(source_class, from_class, to_class, group_key)
+        puts "from_class: #{from_class}, to_class: #{to_class}, group_key: #{group_key}"
+        @boilerplate = Possimatch::Generators::Boilerplate.new(class_name, from_class, to_class, group_key)
         template "possi_resource.rb", "app/models/possimatches/#{file_path.tr('/', '_')}.rb"
       end
     end
