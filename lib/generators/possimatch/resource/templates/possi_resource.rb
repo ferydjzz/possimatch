0c86e0347ef5ffcc47752caa274a91fb7e48d594
FeryJ <fery.japminto@jurnal.id>
2016-06-06 19:57:45 +0700

    class name

--- lib/generators/possimatch/resource/templates/possi_resource.rb
+++ lib/generators/possimatch/resource/templates/possi_resource.rb
@@ -1,4 +1,4 @@
-class <%= get_class_name %> < ActiveRecord::Base
+class <%= @class_name %> < ActiveRecord::Base
   belongs_to <%= @resource_class.tableize.singularize %>
   belongs_to <%= @from_source.tableize.singularize %>
   belongs_to <%= @to_source.tableize.singularize %>
