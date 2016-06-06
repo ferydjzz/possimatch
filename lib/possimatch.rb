c77d24f8e7fd1a1abebe3ec0d57d1855b108b275
FeryJ <fery.japminto@jurnal.id>
2016-06-06 12:10:36 +0700

    extend configuration module

--- lib/possimatch.rb
+++ lib/possimatch.rb
@@ -2,11 +2,13 @@ require "possimatch/version"
 require "possimatch/helpers/configuration"
 
 module Possimatch
+  
+  # == default value for confirguration
+  extend Configuration
+  define_setting :possible_matches, 3
+  define_setting :stop_after_match, false
+
   class << self
-    # == default value for confirguration
-    define_setting :possible_matches, 3
-    define_setting :stop_after_match, false
-    
     def register_resource(resource_id, from_source, to_source, group_key)
       params = {resource_id: resource_id, from_source: from_source, to_source: to_source, group_key: group_key}
       pr = PossiResource.register(params)
