8dac3cfd7b69e0c89af9912d25be266433d1354a
FeryJ <fery.japminto@jurnal.id>
2016-06-06 19:36:41 +0700

    testing

--- lib/generators/possimatch/resource/resource_generator.rb
+++ lib/generators/possimatch/resource/resource_generator.rb
@@ -10,17 +10,18 @@ module Possimatch
 
       source_root File.expand_path("../templates", __FILE__)
 
-      argument :attributes, :type => :array, :default => [], :banner => "<class_name>[:from_class] <class_name>[:to_class] <group_id>[:group_key]"
+      argument :from_class, :type => :string
+      argument :to_class, :type => :string
+      argument :group_key, :type => :string
 
       def generate_config_file
-        from_class = attributes.select{ |a| a.type == "from_class"}
-        to_class = attributes.select{ |a| a.type == :to_class}
-        group_key = attributes.select{ |a| a.type == :"group_key"}
-
-        puts "#{attributes.each{ |a| [a.name] }}"
-        puts "from_class: #{from_class}, to_class: #{to_class}, group_key: #{group_key}"
-        @boilerplate = Possimatch::Generators::Boilerplate.new(class_name, from_class, to_class, group_key)
-        template "possi_resource.rb", "app/models/possimatches/#{file_path.tr('/', '_')}.rb"
+        puts "from_class #{from_class}"
+        puts "to_class #{to_class}"
+        puts "group_key #{group_key}"
+        # puts "#{attributes.each{ |a| [a.name] }}"
+        # puts "from_class: #{from_class}, to_class: #{to_class}, group_key: #{group_key}"
+        # @boilerplate = Possimatch::Generators::Boilerplate.new(class_name, from_class, to_class, group_key)
+        # template "possi_resource.rb", "app/models/possimatches/#{file_path.tr('/', '_')}.rb"
       end
     end
   end
