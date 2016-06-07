1f00680b7d11e2cdb19e7830dbc27c8ed18e98ee
FeryJ <fery.japminto@jurnal.id>
2016-06-07 18:39:57 +0700

    add to_s before to_sym

--- lib/generators/possimatch/resource/templates/possi_resource.rb
+++ lib/generators/possimatch/resource/templates/possi_resource.rb
@@ -1,3 +1,3 @@
 class <%= @boilerplate.get_class_name %> < Possimatch::PossiResource
-  belongs_to <%= class_name.tableize.singularize.to_sym %>
+  belongs_to <%= class_name.to_s.tableize.singularize.to_sym %>
 end
