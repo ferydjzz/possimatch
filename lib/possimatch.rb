a88bb152a75bb1ac6853c2d203c61e9d47cff8b0
FeryJ <fery.japminto@jurnal.id>
2016-06-07 17:56:44 +0700

    require rails ?

--- lib/possimatch.rb
+++ lib/possimatch.rb
@@ -1,3 +1,4 @@
+require "rails"
 require "possimatch/version"
 require "possimatch/helpers/configuration"
 require "possimatch/app/models/possi_resource.rb"
@@ -12,12 +13,6 @@ module Possimatch
   define_setting :stop_after_match, false
 
   class << self
-    def register_resource(resource_id, from_source, to_source, group_key)
-      params = {resource_id: resource_id, from_source: from_source, to_source: to_source, group_key: group_key}
-      pr = PossiResource.register(params)
-      pr
-    end
-
     def show_resources
     end
   end
--- lib/possimatch/app/models/possi_resource.rb
+++ lib/possimatch/app/models/possi_resource.rb
@@ -1,9 +1,11 @@
 module Possimatch
-  class PossiResource < ActiveRecord::Base
+  class PossiResource < ::ActiveRecord::Base
     validates_presence_of :source_id
     validates_presence_of :from_source
     validates_presence_of :to_source
     validates_presence_of :group_key
+
+    has_many :possi_rules
 
     def build_matcher
       query = "SELECT * FROM transactions 
