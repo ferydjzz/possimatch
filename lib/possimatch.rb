6498b1700e6006f27033fd3c51b4476820f63a2b
FeryJ <fery.japminto@jurnal.id>
2016-06-07 16:34:31 +0700

    change path

--- lib/possimatch.rb
+++ lib/possimatch.rb
@@ -1,8 +1,8 @@
 require "possimatch/version"
 require "possimatch/helpers/configuration"
-require "possimatch/app/models/possi_resource"
-require "possimatch/app/models/possi_rule"
-require "possimatch/app/models/possi_match"
+require "app/models/possi_resource"
+require "app/models/possi_rule"
+require "app/models/possi_match"
 
 module Possimatch
   
