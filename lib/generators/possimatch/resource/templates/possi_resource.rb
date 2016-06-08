9ba456280fa8cc3ba7559db6946278f3756e6529
FeryJ <fery.japminto@jurnal.id>
2016-06-08 10:05:04 +0700

    add to sym

--- lib/generators/possimatch/resource/templates/possi_resource.rb
+++ lib/generators/possimatch/resource/templates/possi_resource.rb
@@ -1,5 +1,5 @@
 class <%= @boilerplate.get_class_name %> < Possimatch::PossiResource
-  belongs_to <%= class_name.to_s.tableize.singularize.to_sym %>
+  belongs_to <%= "#{class_name.tableize.singularize}".to_sym %>
 
   def self.source_class
     <%= class_name.classify %>
