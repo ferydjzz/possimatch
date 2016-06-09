ea58263665aa877f20cb5d527ea0ea17c3fc3897
FeryJ <fery.japminto@jurnal.id>
2016-06-09 14:40:01 +0700

    add some comment in possi resource template

--- lib/generators/possimatch/resource/templates/possi_resource.rb
+++ lib/generators/possimatch/resource/templates/possi_resource.rb
@@ -1,6 +1,27 @@
 class <%= @boilerplate.get_class_name %> < Possimatch::PossiResource
   belongs_to :<%= "#{class_name.tableize.singularize}" %>
 
+# == automatic create resource when new <%= "#{class_name.tableize.singularize}" %> created
+# add this callback and function to class <%= "#{class_name.classify}" %>
+# 
+# after_create :create_possisource
+#
+# def create_possisource
+#   <%= @boilerplate.get_class_name %>.create(source_id: self.id)
+# end
+#
+# == automatic create default rules when resource created
+# callback to create default rules
+#
+# after_create :create_default_rules
+# 
+# def create_default_rules
+#   self.possi_rules.create(from_source_field: '<field in <%= @from_class.tableize %>>', 
+#                         to_source_field: '<field in <%= @to_class.tableize %>>',
+#                         group_key: '<field in both class to be identifier>')
+# end
+#
+
   def self.source_class
     <%= class_name.classify %>
   end
