9de5104451dc9bcad3e2f8632e6a598b32c1e25a
FeryJ <fery.japminto@jurnal.id>
2016-06-10 16:13:50 +0700

    add source id to possi matches

--- lib/generators/possimatch/install/templates/migrations/create_possi_matches.rb
+++ lib/generators/possimatch/install/templates/migrations/create_possi_matches.rb
@@ -1,12 +1,13 @@
 class CreatePossiMatches < ActiveRecord::Migration
   def self.up
     create_table :possi_matches do |t|
+      t.integer  :source_id
       t.integer  :from_source_id
       t.integer  :to_source_id
       t.decimal  :score
       t.timestamps
     end
-    add_index :possi_matches, [:from_source_id, :to_source_id, :score], order: {from_source_id: :asc, to_source_id: :asc, score: :desc}
+    add_index :possi_matches, [:source_id, :from_source_id, :to_source_id, :score], name: "idx_possi_matches"
   end
 
   def self.down
