02dc288fc86811917c067497da70969c16153f13
haryantowang <haryantowang09@gmail.com>
2016-06-15 16:12:22 +0700

    fix update

--- lib/generators/possimatch/resource/templates/models/possi_match.rb
+++ lib/generators/possimatch/resource/templates/models/possi_match.rb
@@ -2,7 +2,7 @@ class PossiMatch < Possimatch::PossiMatch
   belongs_to :<%= "#{@from_class.tableize.singularize}" %>
   belongs_to :<%= "#{@to_class.tableize.singularize}" %>
 
-  def get_possible_matches_data(source_id)
+  def self.get_possible_matches_data(source_id)
     PossiMatch.all.where("source_id = ?", source_id)
   end
 end
