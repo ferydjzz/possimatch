34fa6cf2f070e311216fb53296df9df453fd9f82
FeryJ <fery.japminto@jurnal.id>
2016-06-16 18:14:03 +0700

    abstract away method

--- lib/generators/possimatch/resource/templates/models/possi_match.rb
+++ lib/generators/possimatch/resource/templates/models/possi_match.rb
@@ -2,8 +2,4 @@ class PossiMatch < Possimatch::PossiMatch
   belongs_to :from_source, class_name: '<%= @from_class.tableize.singularize %>'
   belongs_to :to_source, class_name: '<%= @to_class.tableize.singularize %>'
   belongs_to :source, class_name: '<%= @group_key.split("_id").first.classify %>' 
-
-  def self.get_possible_matches_data(source_id)
-    PossiMatch.all.where("source_id = ?", source_id)
-  end
 end
--- lib/possimatch/app/models/possi_match.rb
+++ lib/possimatch/app/models/possi_match.rb
@@ -4,4 +4,8 @@ module Possimatch
     validates_presence_of :from_source_id
     validates_presence_of :to_source_id
   end
+
+  def self.get_possible_matches_data(source_id)
+    self.all.where("source_id = ?", source_id)
+  end
 end
