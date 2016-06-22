7ff2cb91c6b8f2f1c38e2068b63e72fe0be34d19
FeryJ <fery.japminto@jurnal.id>
2016-06-22 15:45:58 +0700

    add classify

--- lib/generators/possimatch/resource/templates/models/possi_match.rb
+++ lib/generators/possimatch/resource/templates/models/possi_match.rb
@@ -1,5 +1,5 @@
 class PossiMatch < Possimatch::PossiMatch
-  belongs_to :from_source, class_name: '<%= @from_class.tableize.singularize %>'
-  belongs_to :to_source, class_name: '<%= @to_class.tableize.singularize %>'
+  belongs_to :from_source, class_name: '<%= @from_class.classify %>'
+  belongs_to :to_source, class_name: '<%= @to_class.classify %>'
   belongs_to :source, class_name: '<%= @group_key.split("_id").first.classify %>' 
 end
