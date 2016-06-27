54829efa938959c4954b6600f66c8820ed14f0d0
FeryJ <fery.japminto@jurnal.id>
2016-06-27 11:22:10 +0700

    fix missplace in possimatch model

--- lib/possimatch/app/models/possi_match.rb
+++ lib/possimatch/app/models/possi_match.rb
@@ -1,11 +1,11 @@
 module Possimatch
   class PossiMatch < ::ActiveRecord::Base
-  	validates_presence_of :source_id
+    validates_presence_of :source_id
     validates_presence_of :from_source_id
     validates_presence_of :to_source_id
-  end
-
-  def self.get_possible_matches_data(source_id)
-    self.all.where("source_id = ?", source_id)
+  
+    def self.get_possible_matches_data(source_id)
+      self.all.where("source_id = ?", source_id)
+    end
   end
 end
